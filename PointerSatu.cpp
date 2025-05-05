8b89f078711ba398465a59a1f8575940e084d353
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-05 10:54:58 +0700

    fix n create int main n reference

--- PointerSatu.cpp
+++ PointerSatu.cpp
@@ -1,3 +1,25 @@
 #include <iostream>
 using namespace std;
 class mahasiswa
+{
+public:
+    int nim;
+
+        void
+        showNim()
+    {
+        cout << "No Induk ="  <<nim<<endl;
+    }
+};
+
+int
+main()
+{
+
+    mahasiswa mhs{1};
+    mhs.showNim();
+
+    mahasiswa &refMhs = mhs;
+    refMhs.nim = 2;
+    mhs.showNim();
+}
