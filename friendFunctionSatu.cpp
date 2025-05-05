debb9e2c23f7d849e1f9ee78ba216afc0acd5d20
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-05 11:27:51 +0700

    make void n int main mahasiswa (joko)

--- friendFunctionSatu.cpp
+++ friendFunctionSatu.cpp
@@ -4,9 +4,21 @@ using namespace std;
 
 class mahasiswa
 {
-    private:
+private:
     string nama;
 
-    public:
+public:
     friend void setNama(mahasiswa &a, string);
 };
+
+void setNama(mahasiswa &a, string b)
+{
+    a.nama = b;
+    cout << a.nama;
+}
+int main()
+{
+    mahasiswa joko;
+    setNama(joko, "Kairi Kumar");
+    return 0;
+}
