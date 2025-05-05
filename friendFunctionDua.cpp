2d6931e27ad3c9c8716abb00c88c6808c73c78da
wpiskaa <hafizzkurniawan19@gmail.com>
2025-05-05 11:34:33 +0700

    member class in class

--- friendFunctionDua.cpp
+++ friendFunctionDua.cpp
@@ -3,3 +3,17 @@ using namespace std;
 
 class pelajar;
 class manusia
+{
+    public:
+    void showNilauPelajar(pelajar &x);
+    //void showNilaiPelajar2(pelajar &x);
+};
+
+class pelajar
+{
+    private:
+    int nilai;
+
+    public: pelajar(){nilai = 100;}
+    friend void manusia::showNilauPelajar(pelajar &x);
+}
